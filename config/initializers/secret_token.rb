c4e002e4d9d51551a48a832f141e9cf3887124b8
Daniel Salmeron Amselem <daniel.amselem@gmail.com>
2013-01-31 17:50:43 +0200

    Hard code secret token temporarily.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,8 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 
-if ENV['RAILS_SECRET_TOKEN']
-  Newsletter::Application.config.secret_token = ENV['RAILS_SECRET_TOKEN']
-else
-  raise 'You have to define ENV["RAILS_SECRET_TOKEN"]'
-end
+Newsletter::Application.config.secret_token = '08a25b8059d020f4763dfe8aad56ed151bfe90cfc181f6c595ad14919383fd5575e361298784be7e261fe70f104c082600261c61ed4092a72a31be9a2da4c3b7'
